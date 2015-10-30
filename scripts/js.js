6b1a96dea56ff180fd437b2c68023dc460d8457f
Kevin Moore <kevmoo@google.com>
2015-10-30 09:09:33 -0700

    adding support for prettify

---
+++ scripts/js.js
@@ -0,0 +1,19 @@
+// Source: https://gist.github.com/nilliams/7138983
+var hasUpdates = false;
+[].forEach.call(document.querySelectorAll("pre code"), function(el) {
+  hasUpdates = true;
+  el.classList.add("prettyprint");
+});
+
+if (hasUpdates) {
+  console.log("pretty");
+  var link = document.createElement('link');
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.href = '//cdn.rawgit.com/google/code-prettify/master/loader/prettify.css'
+
+  document.head.appendChild(link)
+  prettyPrint();
+} else {
+  console.log('no pretty');
+}
